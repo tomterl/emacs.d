ce875c8510d48f4d2dec18fe55dfcbbe50937c72
Tom Regner <tom@goochesa.de>
2013-12-22 21:09:51 +0100

    Restructure and cleanup the init.el file.

--- init.el
+++ init.el
@@ -1,23 +1,24 @@
 ;;; init.el --- Where all the magic begins
 ;;
 ;; 
+;;; Commentary:
 ;;
+;;; Code:
+;; load Org-mode and gnus from source.
 
-;; load Org-mode and gnus from source 
+;; Variables
+(defvar tom/--emacs-dir (expand-file-name "~/.emacs.d")
+  "Directory of the Emacs setup.")
+(defvar tom/--src-base (expand-file-name "~/.emacs.src.d")
+  "Location of local repositories.")
+(defvar tom/--org-base (expand-file-name "org-mode" tom/--src-base)
+  "Location of `org-mode'.")
 
-(setq tom/--emacs-dir (expand-file-name "~/.emacs.d")
-      tom/--src-base (expand-file-name "~/.emacs.src.d"))
-
-(setq tom/--org-home (expand-file-name "org-mode" tom/--src-base))
-
-(let ((org-lisp-dir (expand-file-name "lisp" tom/--org-home)))
+(let ((org-lisp-dir (expand-file-name "lisp" tom/--org-base)))
   (when (file-directory-p org-lisp-dir)
     (add-to-list 'load-path org-lisp-dir)
-    (require 'org)))
-
-
-;; load my own customizations 
-(org-babel-load-file (expand-file-name "personal.org" tom/--emacs-dir))
-
+    (require 'org)
+    ;; Load my own customizations.
+    (org-babel-load-file (expand-file-name "personal.org" tom/--emacs-dir))))
 
 ;;; init.el ends here
