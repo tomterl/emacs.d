c8d41bf487e82b79a3efd8d5be76ac8e2958b204
Tom Regner <tom@goochesa.de>
2014-03-13 06:18:44 +0100

    Only tangle/compile when needed.

--- init.el
+++ init.el
@@ -13,14 +13,23 @@
 (defvar tom/--org-base (expand-file-name "org-mode" tom/--src-base)
   "Location of `org-mode'.")
 
+;; Code
+(require 'cl-lib)
 (let ((org-lisp-dir (expand-file-name "lisp" tom/--org-base)))
-  (when (file-directory-p org-lisp-dir)
-    (add-to-list 'load-path org-lisp-dir)
-    (require 'org)
-    ;; Load my own customizations.
-    (let ((org-file (expand-file-name "personal.org" tom/--emacs-dir))
-          (el-file (expand-file-name "personal.el" tom/--emacs-dir)))
-      (org-babel-tangle-file org-file el-file "emacs-lisp")
-      (byte-compile-file el-file t))))
+  (cl-flet ((age (file)
+                 (float-time
+                  (time-subtract (current-time)
+                                 (nth 5 (or (file-attributes (file-truename file))
+                                            (file-attributes file)))))))
+    (when (file-directory-p org-lisp-dir)
+      (add-to-list 'load-path org-lisp-dir)
+      (require 'org)
+      ;; Load my own customizations.
+      (let ((org-file (expand-file-name "personal.org" tom/--emacs-dir))
+            (el-file (expand-file-name "personal.el" tom/--emacs-dir)))
+        (when (not (and (file-exists-p el-file)
+                        (> (age org-file) (age el-file))))
+          (org-babel-tangle-file org-file el-file "emacs-lisp")
+          (byte-compile-file el-file t))))))
 
 ;;; init.el ends here
