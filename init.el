3fc5ea3a1fdf99131bf36b388238b3fe867682ce
Tom Regner <thomas.regner@joocom.de>
2014-03-28 09:27:11 +0100

    org does the age check in org-babel-load-file.

--- init.el
+++ init.el
@@ -15,23 +15,13 @@
 
 ;; Code
 (require 'cl-lib)
+
+
 (let ((org-lisp-dir (expand-file-name "lisp" tom/--org-base)))
-  (cl-flet ((age (file)
-                 (float-time
-                  (time-subtract (current-time)
-                                 (nth 5 (or (file-attributes (file-truename file))
-                                            (file-attributes file)))))))
-    (when (file-directory-p org-lisp-dir)
-      (add-to-list 'load-path org-lisp-dir)
-      (require 'org)
-      ;; Load my own customizations.
-      (let ((org-file (expand-file-name "personal.org" tom/--emacs-dir))
-            (el-file (expand-file-name "personal.el" tom/--emacs-dir))
-            (elc-file (expand-file-name "personal.elc" tom/--emacs-dir)))
-        (when (not (and (file-exists-p el-file)
-                        (> (age org-file) (age el-file))))
-          (org-babel-tangle-file org-file el-file "emacs-lisp")
-          (byte-recompile-file el-file t))
-        (load-file elc-file)))))
+   (let ((org-file (expand-file-name "personal.org" tom/--emacs-dir)))
+     (when (file-directory-p org-lisp-dir)
+       (add-to-list 'load-path org-lisp-dir)
+       (require 'org)
+       (org-babel-load-file org-file))))
 
 ;;; init.el ends here
