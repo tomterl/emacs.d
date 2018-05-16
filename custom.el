d4a3e2d4fc235558fad98544b2ed81c0db9121be
Tom Regner <tom@goochesa.de>
2018-05-16 15:12:58 +0200

    New packages.

--- custom.el
+++ custom.el
@@ -21,25 +21,7 @@
    '("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "6de7c03d614033c0403657409313d5f01202361e35490a3404e33e46663c2596" "87233846530d0b2c50774c74c4aca06a1472504c63ccd4ab2b1021b3e56a69e9" "d9129a8d924c4254607b5ded46350d68cc00b6e38c39fc137c3cfb7506702c12" "9b402e9e8f62024b2e7f516465b63a4927028a7055392290600b776e4a5b9905" "227edf860687e6dfd079dc5c629cbfb5c37d0b42a3441f5c50873ba11ec8dfd2" "47aa6e82734866b2915781c6e1d9517bd897d45fe8aec360dd4b6294fec73068" "e239877fd4078f3d199e92482295f69d8ddc9f9ec0331f09692c0d5b7c13d908" "71669abbe2a456288ba38afbba1ce731f5a0b17ef146c7d312dd1d9229857773" "a3f85ee6e877f02e239d2a6633a5b8263b53113751aca549aa4c5f458829c95d" "5e3fc08bcadce4c6785fc49be686a4a82a356db569f55d411258984e952f194a" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "5dc0ae2d193460de979a463b907b4b2c6d2c9c4657b2e9e66b8898d2592e3de5" "7153b82e50b6f7452b4519097f880d968a6eaf6f6ef38cc45a144958e553fbc6" "ba9be9caf9aa91eb34cf11ad9e8c61e54db68d2d474f99a52ba7e87097fa27f5" "af53ecfbc4ceca20dc5bfc66c88544a216324bee58507c2624dda25cde7e98f4" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "bc40f613df8e0d8f31c5eb3380b61f587e1b5bc439212e03d4ea44b26b4f408a" "95db78d85e3c0e735da28af774dfa59308db832f84b8a2287586f5b4f21a7a5b" "a0feb1322de9e26a4d209d1cfa236deaf64662bb604fa513cca6a057ddf0ef64" "ab04c00a7e48ad784b52f34aa6bfa1e80d0c3fcacc50e1189af3651013eb0d58" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" default))
  '(diary-entry-marker 'font-lock-variable-name-face)
  '(emms-mode-line-icon-image-cache
-   '(image :type xpm :ascent center :data "/* XPM */
-static char *note[] = {
-/* width height num_colors chars_per_pixel */
-\"    10   11        2            1\",
-/* colors */
-\". c #1fb3b3\",
-\"# c None s None\",
-/* pixels */
-\"###...####\",
-\"###.#...##\",
-\"###.###...\",
-\"###.#####.\",
-\"###.#####.\",
-\"#...#####.\",
-\"....#####.\",
-\"#..######.\",
-\"#######...\",
-\"######....\",
-\"#######..#\" };"))
+   '(image :type xpm :ascent center :data "/* XPM */\nstatic char *note[] = {\n/* width height num_colors chars_per_pixel */\n\"    10   11        2            1\",\n/* colors */\n\". c #1fb3b3\",\n\"# c None s None\",\n/* pixels */\n\"###...####\",\n\"###.#...##\",\n\"###.###...\",\n\"###.#####.\",\n\"###.#####.\",\n\"#...#####.\",\n\"....#####.\",\n\"#..######.\",\n\"#######...\",\n\"######....\",\n\"#######..#\" };"))
  '(es-always-pretty-print t)
  '(es-cc-endpoint "http://192.168.16.249:9200/")
  '(es-default-url "http://192.168.16.249:9200/_search?pretty=true")
@@ -50,27 +32,7 @@ static char *note[] = {
  '(ggtags-highlight-tag-delay 1.0)
  '(gnus-logo-colors '("#528d8d" "#c0c0c0") t)
  '(gnus-mode-line-image-cache
-   '(image :type xpm :ascent center :data "/* XPM */
-static char *gnus-pointer[] = {
-/* width height num_colors chars_per_pixel */
-\"    18    13        2            1\",
-/* colors */
-\". c #1fb3b3\",
-\"# c None s None\",
-/* pixels */
-\"##################\",
-\"######..##..######\",
-\"#####........#####\",
-\"#.##.##..##...####\",
-\"#...####.###...##.\",
-\"#..###.######.....\",
-\"#####.########...#\",
-\"###########.######\",
-\"####.###.#..######\",
-\"######..###.######\",
-\"###....####.######\",
-\"###..######.######\",
-\"###########.######\" };") t)
+   '(image :type xpm :ascent center :data "/* XPM */\nstatic char *gnus-pointer[] = {\n/* width height num_colors chars_per_pixel */\n\"    18    13        2            1\",\n/* colors */\n\". c #1fb3b3\",\n\"# c None s None\",\n/* pixels */\n\"##################\",\n\"######..##..######\",\n\"#####........#####\",\n\"#.##.##..##...####\",\n\"#...####.###...##.\",\n\"#..###.######.....\",\n\"#####.########...#\",\n\"###########.######\",\n\"####.###.#..######\",\n\"######..###.######\",\n\"###....####.######\",\n\"###..######.######\",\n\"###########.######\" };") t)
  '(golden-ratio-mode nil)
  '(haskell-process-auto-import-loaded-modules t)
  '(haskell-process-log t)
@@ -109,7 +71,7 @@ static char *gnus-pointer[] = {
  '(main-line-separator-style 'chamfer)
  '(menu-bar-mode nil)
  '(package-selected-packages
-   '(company-box company-quickhelp moe-theme kaleidoscope ido-completing-read+ persp-mode persp-mode-projectile-bridge rg speed-type ob-http smart-mode-line-powerline-theme link-hint sql-indent ido-vertical-mode dracula-theme badger-theme flyspell-correct urlenc railscasts-reloaded-theme creamsody-theme material-theme zeal-at-point company-php outline-magic firebelly-theme diminish notify company-racer company-tern flycheck-rust racer rustfmt nlinum paradox php-refactor-mode ac-php httpd inf-groovy magit-tramp elixir-mix yagist xml-rpc wrap-region window-layout wgrep-ag volatile-highlights vagrant underwater-theme twilight-bright-theme twilight-anti-bright-theme tracking sunrise-x-mirror sunrise-x-loop stylus-mode stumpwm-mode string-inflection sr-speedbar soothe-theme smooth-scrolling smex smart-cursor-color ruby-tools ruby-test-mode ruby-refactor ruby-end ruby-dev rotate rinari repl-toggle popwin popup phpunit php-auto-yasnippets paredit pallet origami omni-scratch names minimap lua-mode lentic json-rpc jade-mode jabber ix impatient-mode image+ httprepl handlebars-mode grunt groovy-mode golden-ratio go-errcheck go-eldoc go-direx git-timemachine fringe-helper flymake-yaml flymake-elixir flymake-cursor flx-isearch flx-ido flatland-theme expand-region erc-tweet epresent elnode elixir-yasnippets edbi drag-stuff dockerfile-mode db-pg csv-mode company-inf-ruby color-theme-approximate coffee-mode clues-theme centered-cursor-mode caskxy ample-theme alect-themes alchemist aggressive-indent ag ace-window ace-jump-buffer 2048-game))
+   '(fountain-mode wc-mode writeroom-mode ob-rust color-theme-sanityinc-solarized company-box company-quickhelp moe-theme kaleidoscope ido-completing-read+ persp-mode persp-mode-projectile-bridge rg speed-type ob-http smart-mode-line-powerline-theme link-hint sql-indent ido-vertical-mode dracula-theme badger-theme flyspell-correct urlenc railscasts-reloaded-theme creamsody-theme material-theme zeal-at-point company-php outline-magic firebelly-theme diminish notify company-racer company-tern flycheck-rust racer rustfmt nlinum paradox php-refactor-mode ac-php httpd inf-groovy magit-tramp elixir-mix yagist xml-rpc wrap-region window-layout wgrep-ag volatile-highlights vagrant underwater-theme twilight-bright-theme twilight-anti-bright-theme tracking sunrise-x-mirror sunrise-x-loop stylus-mode stumpwm-mode string-inflection sr-speedbar soothe-theme smooth-scrolling smex smart-cursor-color ruby-tools ruby-test-mode ruby-refactor ruby-end ruby-dev rotate rinari repl-toggle popwin popup phpunit php-auto-yasnippets paredit pallet origami omni-scratch names minimap lua-mode lentic json-rpc jade-mode jabber ix impatient-mode image+ httprepl handlebars-mode grunt groovy-mode golden-ratio go-errcheck go-eldoc go-direx git-timemachine fringe-helper flymake-yaml flymake-elixir flymake-cursor flx-isearch flx-ido flatland-theme expand-region erc-tweet epresent elnode elixir-yasnippets edbi drag-stuff dockerfile-mode db-pg csv-mode company-inf-ruby color-theme-approximate coffee-mode clues-theme centered-cursor-mode caskxy ample-theme alect-themes alchemist aggressive-indent ag ace-window ace-jump-buffer 2048-game))
  '(paradox-automatically-star nil)
  '(persp-add-buffer-on-after-change-major-mode 'free)
  '(persp-add-buffer-on-find-file nil)
